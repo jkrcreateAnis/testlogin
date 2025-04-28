f32d074ce97318d11835d0b9b72a35fd971b55da
jkrcreateAnis <anissolehah2000@gmail.com>
2025-04-28 09:12:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from authlib.integrations.requests_client import OAuth2Session
+# from authlib.integrations.requests_client import OAuth2Session
 
 st.title("Authentication")
 if st.button("Authenticate"):
